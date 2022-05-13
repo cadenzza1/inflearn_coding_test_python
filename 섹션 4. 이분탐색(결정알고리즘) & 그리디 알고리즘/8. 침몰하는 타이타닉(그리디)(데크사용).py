a30fc0397701266ea9b549be824e03536bed50b4
cadenzza1 <minq0302@naver.com>
2022-05-13 17:03:10 +0900

    Solved Section4 8.침몰하는 타이타닉(데크사용)

---
+++ 섹션 4. 이분탐색(결정알고리즘) & 그리디 알고리즘/8. 침몰하는 타이타닉(그리디)(데크사용).py
@@ -0,0 +1,26 @@
+from collections import deque
+
+n, m = map(int,input().split())
+arr = list(map(int,input().split()))
+
+arr.sort()
+arr = deque(arr) # 리스트를 데크 자료구조로 변경
+cnt = 0 # 구명보트의 개수
+
+while arr: # 배열이 비어있지 않으면 계속 반복문 실행
+    if len(arr) == 1: # 최후에 한 명만 남았을 시 그냥 태워 보내깅
+        cnt += 1
+        break 
+    if arr[0] + arr[-1] > m:
+        arr.pop() # 둘의 무게를 합친게 제한보다 크면 젤 무거운놈만 태워 보내기 (pop시키기)
+        cnt += 1
+    else:
+        arr.popleft() # !리스트와의 차이점! --> 효율적
+        arr.pop()
+        cnt += 1
+
+print(cnt)
+
+# 리스트를 사용하면 젤 앞의 요소를 pop할 때마다 뒤의 요소들이 한 칸씩 앞으로 땡겨짐 --> 상당히 비효율적
+# '데크'라는 자료구조를 사용하면 이런 땡겨지는 연산 없이, pop되면 알아서 포인터가 다음 요소를 가리키도록 변경됨
+# 그래서 데크 자료구조도 사용해서 구현해봤습니다.
