32ab53049e5254ca3a9aea60e4614569410f5beb
cadenzza1 <minq0302@naver.com>
2022-06-12 18:09:14 +0900

    Solved Section6 2.이진트리순회

---
+++ 섹션 6. 완전탐색(백트랙킹, 상태트리와 CUT EDGE) DFS 기초)/2. 이진트리순회(깊이우선탐색).py
@@ -0,0 +1,27 @@
+def preorder(v):
+    if v > 7:
+        return
+    else:
+        print(v, end = " ")
+        preorder(v*2)
+        preorder(v*2 +1)
+
+def inorder(v):
+    if v > 7:
+        return
+    else:
+        inorder(v*2)
+        print(v, end = " ")
+        inorder(v*2 +1)
+
+def postorder(v):
+    if v > 7:
+        return
+    else:
+        postorder(v*2)
+        postorder(v*2 +1)
+        print(v, end = " ")
+
+preorder(1)
+inorder(1)
+postorder(1)
